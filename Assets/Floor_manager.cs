58c456940e25d901113a7db3deacc8255357b01b
Timvandeneng <t.van.den.eng@sintlucasedu.nl>
2023-12-22 14:15:42 +0100

    Long pants added trash also spawns upstairs

--- Assets/Floor_manager.cs
+++ Assets/Floor_manager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Floor_manager : MonoBehaviour
 {
 
-    public GameObject topfloor, Fogvol;
+    public GameObject topfloor, Fogvol, trash;
     public bool up;
 
     // Start is called before the first frame update
@@ -20,11 +20,13 @@ public class Floor_manager : MonoBehaviour
         if (up)
         {
             topfloor.SetActive(true);
+            trash.SetActive(true);
             Fogvol.SetActive(true);
         }
         else
         {
             topfloor.SetActive(false);
+            trash.SetActive(false);
             Fogvol.SetActive(false);
         }
     }
